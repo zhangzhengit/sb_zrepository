9c15b66281deca1cc4de0b9ec6eabbcccf484d09
zhangzhen <1071324756@qq.com>
2023-09-04 18:36:24 +0800

    TODO 程序启动时，就判断字段是否合理，如类型对不对、@ZID字段是否主键等等

--- sb_zrepository/src/main/java/com/vo/anno/ZEntity.java
+++ sb_zrepository/src/main/java/com/vo/anno/ZEntity.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Component;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE })
+//FIXME 2023年9月4日 下午6:34:55 zhanghen: TODO 程序启动时，就判断字段是否合理，如类型对不对、@ZID字段是否主键等等
 public @interface ZEntity {
 
 	// FIXME 2023年6月15日 下午1:45:52 zhanghen: 支持分库分表
